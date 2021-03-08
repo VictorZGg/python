95a927830bdbcc1f0fc436e68ffc3f399cc5c420
VictorZGg <zengguang225@163.com>
2021-03-08 11:15:55 +0800

    web

--- web/news.py
+++ web/news.py
@@ -267,7 +267,7 @@ for i in t3:
     except:
         pass
 ###################### 全部导出至excel文件 ######################
-table.to_excel('D:\\ZG\\tools\\python\\python\\web\\news.xlsx',index=False,header=True)
+table.to_excel('D:\\tools\\python\\python\\web\\news.xlsx',index=False,header=True)
     
 
     
