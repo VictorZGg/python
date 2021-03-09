787d983e5e7a2680e662a9fe0b91ad244ed04b42
VictorZGg <zengguang225@163.com>
2021-03-09 22:30:45 +0800

    Update heima_day01.py

--- machinelearning/heima_day01.py
+++ machinelearning/heima_day01.py
@@ -1,11 +1,19 @@
 from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
 
 def datasets_demo():
-    # sklearn数据集使用
-    # 获取数据集
+    # sklearn数据集更新
+    # 获取数据
+    
     iris = load_iris()
     print ("莺尾花数据集：\n", iris)
-    print ("莺尾花数据集描述：\n", iris["DESCR"])
+    print ("查看数据集描述：\n", iris["DESCR"])
+    print ("查看特征名：\n", iris.feature_names)
+    print ("查看特征值：\n", iris.data, iris.data.shape)
+
+    # 数据集划分
+    x_train, x_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.2, random_state=22)
+    print ("训练集的特征值：\n", x_train, x_train.shape)
     return None
 
 if __name__ == "__main__":
